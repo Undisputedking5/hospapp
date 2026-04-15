f18175ca7a0e87f37f175b7dda9e99c6009dab64
Undisputedking5 <grayhawi2008@gmail.com>
2026-04-15 08:48:02 +0300

    patientviewmodel

--- app/src/main/java/com/example/hospitalmanagementsystem/navigations/AppNavHost.kt
+++ app/src/main/java/com/example/hospitalmanagementsystem/navigations/AppNavHost.kt
@@ -7,15 +7,18 @@ import androidx.navigation.compose.rememberNavController
 import com.example.hospitalmanagementsystem.ui.theme.screens.register.RegisterScreen
 import androidx.navigation.compose.composable
 import com.example.hospitalmanagementsystem.ui.theme.screens.login.LoginScreen
+import com.example.hospitalmanagementsystem.ui.theme.screens.patient.AddPatientScreen
 import com.example.tma.ui.theme.screens.dashboard.DashboardScreen
 
 @Composable
 fun AppNavHost(navController: NavHostController = rememberNavController(),
-               startDestination: String = ROUTE_REGISTER){
+               startDestination: String = ROUTE_ADD_PATIENT){
     NavHost(navController = navController,
         startDestination = startDestination) {
         composable(ROUTE_REGISTER){ RegisterScreen(navController) }
         composable(ROUTE_LOGIN) {LoginScreen(navController)}
         composable(ROUTE_DASHBOARD) { DashboardScreen(navController) }
+        composable(ROUTE_ADD_PATIENT) { AddPatientScreen(navController)}
+
     }
 }
--- app/src/main/java/com/example/hospitalmanagementsystem/navigations/Routes.kt
+++ app/src/main/java/com/example/hospitalmanagementsystem/navigations/Routes.kt
@@ -3,3 +3,6 @@ package com.example.hospitalmanagementsystem.navigations
 const val ROUTE_REGISTER = "register"
 const val ROUTE_LOGIN = "login"
 const val ROUTE_DASHBOARD = "dashboard"
+
+const val ROUTE_ADD_PATIENT = "add_patient"
+
